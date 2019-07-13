282eee956841f4fb521e27426501eedac8e81f46
DragonKnightOfBreeze <2944262241@qq.com>
2019-07-13 16:46:42 +0800

    1.0.5 更新Mod概述，修正部分过时的种族资料翻译（官方&FU）。

--- release/pack.bat
+++ release/pack.bat
@@ -1,5 +1,4 @@
-rem 定位到asset.packer.exe
-D:
-cd Programs\Steam\steamapps\common\Starbound\win32
-rem 打包mod
+REM 定位到asset.packer.exe
+cd D:\\Programs\Steam\steamapps\common\Starbound\win32
+REM 打包mod
 asset_packer.exe "D:\OneDrive\My Documents\My Projects\Manage\FrackinRaces-sChinese-Project\package" "D:\OneDrive\My Documents\My Projects\Manage\FrackinRaces-sChinese-Project\release\FR SChinese.pak"
